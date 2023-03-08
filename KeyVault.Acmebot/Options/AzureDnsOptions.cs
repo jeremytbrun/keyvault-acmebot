0c77494aa80202c47f6b9a527fef48b3a840044b
Jeremy Brun <JerBrun0515@gmail.com>
2023-03-08 15:38:53 -0500

    Update AzureDnsOptions.cs

--- KeyVault.Acmebot/Options/AzureDnsOptions.cs
+++ KeyVault.Acmebot/Options/AzureDnsOptions.cs
@@ -3,4 +3,8 @@
 public class AzureDnsOptions
 {
     public string SubscriptionId { get; set; }
+    public string AzureEnvironmentName { get; set; }
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+    public string TenantId { get; set; }
 }
